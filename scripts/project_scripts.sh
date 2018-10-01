3ebe5ec28b85935e16e3a60b7e3a7604a6d29d10
Danny deSousa <ddesousa@lumerical.com>
2018-10-01 10:33:08 -0700

    Removed reference of organization.

--- scripts/project_scripts.sh
+++ scripts/project_scripts.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-PROJECT_SCRIPT=
-
-case $PROJECT in
-    lumerical-products)
-        PROJECT_SCRIPT=lumerical-products.sh
-        ;;
-esac
-
-if [ -f $MY_DEV_SCRIPTS/.projects/$PROJECT_SCRIPT ]; then
-    source $MY_DEV_SCRIPTS/.projects/$PROJECT_SCRIPT
+if [ -f $MY_DEV_SCRIPTS/.projects/$PROJECT.sh ]; then
+    source $MY_DEV_SCRIPTS/.projects/$PROJECT.sh
 fi
